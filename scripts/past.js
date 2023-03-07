7c183dd0a65967cbe666b652ef2e2a3c23633ed9
Alejandro Ruiz Diaz <ajrd21z@gmail.com>
2023-03-07 17:04:45 +0100

    incorporacion de la carpeta scripts con los archivos past.js, upcoming.js, index.js y data.js, que se vinculan con sus respectivos archivos html. Funcion nueva de cards con info dinamica

---
+++ scripts/past.js
@@ -0,0 +1,28 @@
+let contenedorMain = document.getElementById = (main)
+for (events of data.events) {
+    if (events.date < data.currentDate) {
+        console.log(events);
+        let card = document.createElement("div")
+        card.classList.add("card")
+        card.classList.add("cartahover")
+        card.style.width = "18rem"
+        let image = document.createElement("img")
+        image.src = events.image
+        image.classList.add("card-img-top")
+        image.alt = data.events.name
+        let cardBody = document.createElement("div")
+        cardBody.innerHTML = `
+    <div class="card-body align-items-center">
+        <h5 class="card-title text-center">${events.name}</h5>
+        <p class="card-text text-center">${events.description}.</p>
+        <div class="d-flex align-content-center gap-3">
+            <p>Price $ ${events.price}</p>
+            <a href="./cinemadetail.html"class="flex-grow-1 btn btn-outline-light d-flex justify-content-center">Details</a>
+        </div>
+    </div>
+    `
+        card.appendChild(image)
+        card.appendChild(cardBody)
+        contenedorMain.appendChild(card)
+    }
+}
